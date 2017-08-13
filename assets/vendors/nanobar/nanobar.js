d5a0676d1b21a54962b3338d7d94ebb47a183a72
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2017-08-13 23:31:30 +0530

    bower package added, bower vendor destination directory removed from repository

--- assets/vendors/nanobar/nanobar.js
+++ assets/vendors/nanobar/nanobar.js
@@ -1,130 +1,143 @@
-var Nanobar = (function () {
-	'use strict';
-	var addCss, Bar, Nanobar, move, place, init,
-		// container styles
-		cssCont = {
-			width: '100%',
-			height: '4px',
-			zIndex: 9999,
-			top : '0'
-		},
-		// bar styles
-		cssBar = {
-			width:0,
-			height: '100%',
-			clear: 'both',
-			transition: 'height .3s',
-			};
+/* http://nanobar.micronube.com/  ||  https://github.com/jacoborus/nanobar/    MIT LICENSE */
+(function (root) {
+  'use strict'
+  // container styles
+  var css = '.nanobar{width:100%;height:4px;z-index:9999;top:0}.bar{width:0;height:100%;transition:height .3s;background:#000}'
 
+  // add required css in head div
+  function addCss () {
+    var s = document.getElementById('nanobarcss')
 
-	// add `css` to `el` element
-	addCss = function (el, css ) {
-		var i;
-		for (i in css) {
-			el.style[i] = css[i];
-		}
-		el.style.float = 'left';
-	};
+    // check whether style tag is already inserted
+    if (s === null) {
+      s = document.createElement('style')
+      s.type = 'text/css'
+      s.id = 'nanobarcss'
+      document.head.insertBefore(s, document.head.firstChild)
+      // the world
+      if (!s.styleSheet) return s.appendChild(document.createTextNode(css))
+      // IE
+      s.styleSheet.cssText = css
+    }
+  }
 
-	// animation loop
-	move = function () {
-		var self = this,
-			dist = this.width - this.here;
+  function addClass (el, cls) {
+    if (el.classList) el.classList.add(cls)
+    else el.className += ' ' + cls
+  }
 
-		if (dist < 0.1 && dist > -0.1) {
-			place.call( this, this.here );
-			this.moving = false;
-			if (this.width == 100) {
-				this.el.style.height = 0;
-				setTimeout( function () {
-					self.cont.el.removeChild( self.el );
-				}, 300);
-			}
-		} else {
-			place.call( this, this.width - (dist/4) );
-			setTimeout( function () {
-				self.go();
-			}, 16);
-		}
-	};
+  // create a progress bar
+  // this will be destroyed after reaching 100% progress
+  function createBar (rm) {
+    // create progress element
+    var el = document.createElement('div'),
+        width = 0,
+        here = 0,
+        on = 0,
+        bar = {
+          el: el,
+          go: go
+        }
 
-	// set bar width
-	place = function (num) {
-		this.width = num;
-		this.el.style.width = this.width + '%';
-	};
+    addClass(el, 'bar')
 
-	// create and insert bar in DOM and this.bars array
-	init = function () {
-		var bar = new Bar( this );
-		this.bars.unshift( bar );
-	};
+    // animation loop
+    function move () {
+      var dist = width - here
 
-	Bar = function ( cont ) {
-		// create progress element
-		this.el = document.createElement( 'div' );
-		this.el.setAttribute("id", "progressBar");
-		this.el.style.backgroundColor = cont.opts.bg;
-		this.width = 0;
-		this.here = 0;
-		this.moving = false;
-		this.cont = cont;
-		addCss( this.el, cssBar);
-		cont.el.appendChild( this.el );
-	};
+      if (dist < 0.1 && dist > -0.1) {
+        place(here)
+        on = 0
+        if (width === 100) {
+          el.style.height = 0
+          setTimeout(function () {
+            rm(el)
+          }, 300)
+        }
+      } else {
+        place(width - dist / 4)
+        setTimeout(go, 16)
+      }
+    }
 
-	Bar.prototype.go = function (num) {
-		if (num) {
-			this.here = num;
-			if (!this.moving) {
-				this.moving = true;
-				move.call( this );
-			}
-		} else if (this.moving) {
-			move.call( this );
-		}
-	};
+    // set bar width
+    function place (num) {
+      width = num
+      el.style.width = width + '%'
+    }
 
+    function go (num) {
+      if (num >= 0) {
+        here = num
+        if (!on) {
+          on = 1
+          move()
+        }
+      } else if (on) {
+        move()
+      }
+    }
+    return bar
+  }
 
-	Nanobar = function (opt) {
-		var opts = this.opts = opt || {},
-			el;
+  function Nanobar (opts) {
+    opts = opts || {}
+    // set options
+    var el = document.createElement('div'),
+        applyGo,
+        nanobar = {
+          el: el,
+          go: function (p) {
+            // expand bar
+            applyGo(p)
+            // create new bar when progress reaches 100%
+            if (p === 100) {
+              init()
+            }
+          }
+        }
 
-		// set options
-		opts.bg = opts.bg || '#e04040';
-		this.bars = [];
+    // remove element from nanobar container
+    function rm (child) {
+      el.removeChild(child)
+    }
 
+    // create and insert progress var in nanobar container
+    function init () {
+      var bar = createBar(rm)
+      el.appendChild(bar.el)
+      applyGo = bar.go
+    }
 
-		// create bar container
-		el = this.el = document.createElement( 'div' );
-		this.el.setAttribute("id", "progressBarContainer");
-		// append style
-		addCss( this.el, cssCont);
-		if (opts.id) {
-			el.id = opts.id;
-		}
-		// set CSS position
-		el.style.position = !opts.target ? 'fixed' : 'relative';
+    addCss()
 
-		// insert container
-		if (!opts.target) {
-			document.getElementsByTagName( 'body' )[0].appendChild( el );
-		} else {
-			opts.target.insertBefore( el, opts.target.firstChild);
-		}
+    addClass(el, 'nanobar')
+    if (opts.id) el.id = opts.id
+    if (opts.classname) addClass(el, opts.classname)
 
-		init.call( this );
-	};
+    // insert container
+    if (opts.target) {
+      // inside a div
+      el.style.position = 'relative'
+      opts.target.insertBefore(el, opts.target.firstChild)
+    } else {
+      // on top of the page
+      el.style.position = 'fixed'
+      document.getElementsByTagName('body')[0].appendChild(el)
+    }
 
+    init()
+    return nanobar
+  }
 
-	Nanobar.prototype.go = function (p) {
-		// expand bar
-		this.bars[0].go( p );
-
-		// create new bar at progress end
-		if (p == 100) {
-			init.call( this );
-		}
-	};
-	return Nanobar;
-})();
+  if (typeof exports === 'object') {
+    // CommonJS
+    module.exports = Nanobar
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], function () { return Nanobar })
+  } else {
+    // Browser globals
+    root.Nanobar = Nanobar
+  }
+}(this))
