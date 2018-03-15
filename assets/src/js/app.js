8e237dbaec3bef1f1cc0fbfea12ca35e126ed481
Mahapatra <sm98006@nsroot.net>
2018-03-15 18:42:23 +0530

    code cleanup done

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -294,7 +294,8 @@ function initPage() {
 		hours = hours ? hours : 12; // the hour '0' should be '12'
 		minutes = minutes < 10 ? '0' + minutes : minutes;
 		seconds = seconds < 10 ? '0' + seconds : seconds;
-		var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+		//var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+		var strTime = hours + ':' + minutes + ': ' + ampm;
 		return strTime;
 	}
 
