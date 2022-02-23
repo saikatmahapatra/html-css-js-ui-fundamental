c1176f5a259b94e72718a1247d77e8375a4c79f2
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-02-23 08:07:52 +0530

    ES6 tut added

---
+++ src/js/my_app_es6.js
@@ -0,0 +1,3 @@
+import { a, test } from "./test_es6";
+let [...res] = test(a,10);
+console.log(res);
---
+++ src/js/test_es6.js
@@ -0,0 +1,4 @@
+export let a = 10;
+export function test(a, b) {
+    return [(a+b), (a*b), (a-b)];
+}
