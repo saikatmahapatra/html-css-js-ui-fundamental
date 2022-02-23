42961cf63b4fa7ef16a24c5d957ae059ce088235
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-02-23 08:49:24 +0530

    e6 setup related changes

---
+++ src/es6/app.js
@@ -0,0 +1,4 @@
+import { a } from "./test";
+// let [...res] = test(a,10);
+// console.log(res);
+console.log(a);
--- src/js/my_app_es6.js
+++
@@ -1,3 +0,0 @@
-import { a, test } from "./test_es6";
-let [...res] = test(a,10);
-console.log(res);
