b0b8e7c35d3edd04b81774b3262956d5a52cdb43
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-03-20 17:12:37 +0530

    ES6 class created

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,2 +1,14 @@
 //ES6
 const x = 10;
+
+// Class
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+let saikat = new Person('saikat mahapatra');
+console.log(saikat.getName());
