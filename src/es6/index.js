119051056d62fb23411af46146864bff07fc18d5
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-03-25 21:11:15 +0530

    ES6 singleton syntax added

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,3 +21,33 @@ class Person {
 }
 let saikat = new Person('saikat mahapatra');
 console.log(saikat.name);
+
+// Singleton Class
+
+class App {
+    constructor() {
+        if(App._instance) {
+            throw new Error(App._instance+ 'App is a singleton class. It cant be instantiate more than once');
+        }
+
+        App._instance = this;
+    }
+    start(){
+        console.log('running app')
+    }
+}
+
+var app1 = new App();
+var app2 = new App(); // it will throw 
+
+
+// Singleton simple syntax using ES6
+
+let person = new class {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return 'name is' + this.name;
+    }
+}('Saikat Mahapatra');
