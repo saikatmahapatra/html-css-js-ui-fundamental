5b570ec9651ea2e5cd52a8df4e82292330cc4a05
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2024-06-17 17:10:45 +0530

    existing code

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var cleanCSS = require('gulp-clean-css');
-var del = require('del');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
  
-var paths = {
+const paths = {
   styles: {
     src: 'assets/src/styles/**/*.scss',
     dest: 'assets/dist/css/'
@@ -18,19 +18,11 @@ var paths = {
   }
 };
  
-/* Not all tasks need to use streams, a gulpfile is just another node program
- * and you can use all packages available on npm, but it must return either a
- * Promise, a Stream or take a callback and call it
- */
+
 function clean() {
-  // You can use multiple globbing patterns as you would with `gulp.src`,
-  // for example if you are using del 2.0 or above, return its promise
   return del([ 'assets/dist' ]);
 }
- 
-/*
- * Define our tasks using plain functions
- */
+
 function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
@@ -57,20 +49,14 @@ function watch() {
   gulp.watch(paths.styles.src, styles);
 }
  
-/*
- * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
- */
-var build = gulp.series(clean, gulp.series(styles, scripts));
- 
-/*
- * You can use CommonJS `exports` module notation to declare tasks
- */
+
+const build = gulp.series(clean, gulp.series(styles, scripts));
+
 exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watch = watch;
 exports.build = build;
-/*
- * Define default task that can be called by just running `gulp` from cli
- */
+
+// Default task
 exports.default = build;
