a2c55965ec955dc7ce38c5c5cfa233d03ee30f70
Saikat Mahapatra <saikat.mahapatra@accenture.com>
2025-02-11 14:57:40 +0530

    save settings

--- dist/helloworld.js
+++ dist/helloworld.js
@@ -1,3 +1,7 @@
 var message = 'Hello World';
 console.log(message);
+var request = {
+    url: 'https://typescript.tv/',
+    data: 'example',
+};
 //# sourceMappingURL=helloworld.js.map
--- src/ts/helloworld.ts
+++ src/ts/helloworld.ts
@@ -1,2 +1,12 @@
 let message: string = 'Hello World';
 console.log(message);
+
+type CustomRequest<CustomType> = {
+    url: string;
+    data: CustomType;
+};
+
+const request: CustomRequest<string> = {
+    url: 'https://typescript.tv/',
+    data: 'example',
+};
